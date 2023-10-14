debbc2307d0901d683a4441afda9ccbc01ad6033
Ewanderer <jordan.eichner@gmx.de>
2023-10-14 19:01:02 +0200

    code maid cleanup

--- CobaltCoreModding.Definitions/ExternalItems/ExternalStatus.cs
+++ CobaltCoreModding.Definitions/ExternalItems/ExternalStatus.cs
@@ -5,52 +5,9 @@ namespace CobaltCoreModding.Definitions.ExternalItems
 {
     public class ExternalStatus
     {
-
-        private int? id;
-
-        public int? Id
-        {
-            get => id; set
-            {
-                if (id != null)
-                    throw new InvalidOperationException("Object has already id assigned!");
-
-                id = value ?? throw new ArgumentException("Cannot assign null to this field!");
-            }
-        }
-
-        private readonly Dictionary<string, string> name_localisations = new Dictionary<string, string>();
-
         private readonly Dictionary<string, string> desc_localisations = new Dictionary<string, string>();
-
-        public void AddLocalisation(string name, string desc, string localisation = "en")
-        {
-            if (!name_localisations.TryAdd(localisation, name))
-                name_localisations[localisation] = name;
-            if (!desc_localisations.TryAdd(localisation, desc))
-                desc_localisations[localisation] = desc;
-        }
-
-        public void GetLocalisation(string localisation, out string? name, out string? desc) {
-            if (!name_localisations.TryGetValue(localisation, out name))
-                name_localisations.TryGetValue("en", out name);
-            if (!desc_localisations.TryGetValue(localisation, out desc))
-                desc_localisations.TryGetValue("en", out desc);
-        }
-
-        public string GlobalName { get; init; }
-
-        public bool IsGood { get; init; }
-
-        public Color MainColor { get; init; }
-
-        public Color? BorderColor { get; init; }
-
-        private ExternalSprite? icon { get; init; }
-
-        public ExternalSprite Icon => icon ?? throw new InvalidEnumArgumentException("Status was never setup with a sprite.");
-
-        public bool AffectedByTimestop { get; init; }
+        private readonly Dictionary<string, string> name_localisations = new Dictionary<string, string>();
+        private int? id;
 
         public ExternalStatus(string globalName, bool isGood, Color mainColor, Color? borderColor, ExternalSprite icon, bool affectedByTimestop)
         {
@@ -68,10 +25,50 @@ namespace CobaltCoreModding.Definitions.ExternalItems
             GlobalName = "";
         }
 
+        public bool AffectedByTimestop { get; init; }
+
+        public Color? BorderColor { get; init; }
+
+        public string GlobalName { get; init; }
+
+        public ExternalSprite Icon => icon ?? throw new InvalidEnumArgumentException("Status was never setup with a sprite.");
+
+        public int? Id
+        {
+            get => id; set
+            {
+                if (id != null)
+                    throw new InvalidOperationException("Object has already id assigned!");
+
+                id = value ?? throw new ArgumentException("Cannot assign null to this field!");
+            }
+        }
+
+        public bool IsGood { get; init; }
+
+        public Color MainColor { get; init; }
+
+        private ExternalSprite? icon { get; init; }
+
         public static ExternalStatus GetRaw(int id)
         {
             return new ExternalStatus(id);
         }
 
+        public void AddLocalisation(string name, string desc, string localisation = "en")
+        {
+            if (!name_localisations.TryAdd(localisation, name))
+                name_localisations[localisation] = name;
+            if (!desc_localisations.TryAdd(localisation, desc))
+                desc_localisations[localisation] = desc;
+        }
+
+        public void GetLocalisation(string localisation, out string? name, out string? desc)
+        {
+            if (!name_localisations.TryGetValue(localisation, out name))
+                name_localisations.TryGetValue("en", out name);
+            if (!desc_localisations.TryGetValue(localisation, out desc))
+                desc_localisations.TryGetValue("en", out desc);
+        }
     }
 }
--- CobaltCoreModding.Definitions/ModManifests/IGlossaryManifest.cs
+++ CobaltCoreModding.Definitions/ModManifests/IGlossaryManifest.cs
@@ -1,13 +1,8 @@
 ﻿using CobaltCoreModding.Definitions.ModContactPoints;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CobaltCoreModding.Definitions.ModManifests
 {
-    public interface IGlossaryManifest: IManifest
+    public interface IGlossaryManifest : IManifest
     {
         /// <summary>
         /// Called by glossary registry when it times for add extra glossary items into the system.
