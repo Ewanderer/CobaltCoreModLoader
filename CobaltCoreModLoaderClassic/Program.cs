25273d2ce3051edbb0f92e591e80b28300f4224f
Ewanderer <jordan.eichner@gmx.de>
2023-11-23 12:47:32 +0100

    fixing build failures

--- CobaltCoreModLoaderClassic/Program.cs
+++ CobaltCoreModLoaderClassic/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using CobaltCoreModding.Components.Utils;
-using CobaltCoreModdding.Components.Utils;
 
 public static class Program
 {
