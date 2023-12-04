5a9bf05f142fa752825a04779b089321deeee73f
Ewanderer <jordan.eichner@gmx.de>
2023-12-04 14:51:08 +0100

    setup default loop config dict

--- CobaltCoreModding.Components/Services/LoopManagment.cs
+++ CobaltCoreModding.Components/Services/LoopManagment.cs
@@ -9,12 +9,20 @@ namespace CobaltCoreModding.Components.Services
 {
     public class LoopManagment
     {
+
+        private static readonly Dictionary<string, LoopConfiguration> loop_configurations = new Dictionary<string, LoopConfiguration>() {
+            { "default", new LoopConfiguration("default", new string[] {"all" }) },
+            { "all", new LoopConfiguration("all", Array.Empty<string>()) },
+
+        };
+
         /// <summary>
         /// The loop managment has the delicate task to collect configurations
         /// and patch itself into maps and other parts of the game to hook up map pools, events, enemies, etc based on the selected configuration.
         /// </summary>
-        public LoopManagment() {
-           
+        public LoopManagment()
+        {
+
         }
     }
 }
