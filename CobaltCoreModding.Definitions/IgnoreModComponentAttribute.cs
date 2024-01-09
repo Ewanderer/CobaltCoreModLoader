dcfd938475aa2d7063be1bd109f4b1c807af5f2b
Fayti1703 <fayti1703@protonmail.com>
2024-01-09 11:15:28 +0100

    Add AttributeUsage to IgnoreModComponentAttribute

--- CobaltCoreModding.Definitions/IgnoreModComponentAttribute.cs
+++ CobaltCoreModding.Definitions/IgnoreModComponentAttribute.cs
@@ -3,6 +3,7 @@
     /// <summary>
     /// The native loading functions of the mod loader will pass any class with this attribute.
     /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
     public class IgnoreModComponentAttribute : Attribute
     {
     }
